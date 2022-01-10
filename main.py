8586ed11fe7edcbcfabddd651b41fee69222292a
Aleksandr Sheianov <sheyan44@gmail.com>
2022-01-10 17:54:36 +0900

    working

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ def search(query):
     if not main:
         return 'No defenitions found :('
     else:
-
+        examples = reverso_context.get_examples(query, quantity_in_one_step=5)
         final_card = {'word': main[0],
                       'transcription': main[1],
                       'translations': list(reverso_context.translations(query))[:10],
                       'defenition1': '',
                       'exampes1': '',
-                      'reverso_examples': '',
+                      'reverso_examples': next(examples),
                       }
 
-        examples = reverso_context.get_examples(query, quantity_in_one_step=5)
+
         return final_card
 
 
-print(search(input('Enter query: ')))
+print(search(input('Enter a word: ')))
